3104307461afdd113e462bc9959da4a1b7117087
tatatommy6 <tatatommy6@naver.com>
2023-11-04 16:29:16 +0900

    Update RTest.R

--- RTest.R
+++ RTest.R
@@ -1,1 +1,16 @@
-"hello world"
+# d1<-1:50
+# d2<-51:100
+# a=sum(d1)
+# b=sum(d2)
+# a+b
+# # sum(c(d1,d2),na.rm=FALSE)
+# # sum(60<d1<73)
+# 
+# # isFlag=d1>40
+# # list=d1[isFlag]
+# # sum(list,na.rm=TRUE)
+# # sum(d1[60<d1<73],na.rm=TRUE)
+# # sum(d1[d1>60],na.rm=TRUE)
+# v1<-51:90
+# v1[(v1%%2==0)]
+# sum(list,na.rm=TRUE)
